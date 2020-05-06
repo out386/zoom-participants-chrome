b43fc2ed602e0865d6849f50c42cabd37d302c83
Ritayan Chakraborty <ritayanout@gmail.com>
2020-05-06 17:52:53 +0530

    Cleanup

---
+++ src/popup/popup.js
@@ -0,0 +1,64 @@
+chrome.tabs.executeScript({ file: './src/inject.js' });
+
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+    if (sender.tab && request.names) {
+        sendResponse({ resp: "recieved" });
+        showRolls(request.names.split(","))
+    } else {
+        sendResponse({});
+    }
+});
+
+function showRolls(names) {
+    const generalDiv = document.getElementById('general');
+    const foundDiv = document.getElementById('data-found');
+    const missingDiv = document.getElementById('data-missing');
+    let matches = [];
+    let unmatched = [];
+    data = data.map(e => {
+        e.name = e.name.toLowerCase();
+        return e;
+    })
+
+    names.forEach(name => {
+        let info = getName(name);
+        let match = data.find(element => element.name === info.name.toLowerCase());
+        if (match)
+            matches.push({ roll: match.roll, name: info.name });
+        else
+            unmatched.push(name);
+    });
+
+    generalDiv.innerText = `${names.length} names, ${matches.length} found`
+    foundDiv.innerText = `${matches.reduce((acc, n, i) => i > 0 ? `${acc}, ${n.roll}` : n.roll, '')}`;
+    missingDiv.innerText = `${unmatched.reduce((acc, n, i) => i > 0 ? `${acc}, ${n}` : n, '')}`;
+}
+
+/**
+ * Returns an object representating the passed name. The name might or might not have a 
+ * roll number, either at the beginning or at the end. If present, the roll number is
+ * separated from the name with one of the dilimeters: "-_() ". Assumes that there is 
+ * only one sequence of numbers, and that is the roll number.
+ * Example names: "123-Name Abc", "Name Abc (5001)"
+ */
+function getName(item) {
+    let data = { orig: item };
+    let match = item.match(/\d+/);
+    let rollRemoveRegex = /_|-|\(|\)/g;
+
+    if (match) {
+        // A roll number is present
+        data.roll = parseInt(match[0])
+        if (match.index > 0) {
+            // The roll is at the end of the name
+            data.name = item.substring(0, match.index);
+        } else {
+            // The roll is at the beginning of the name
+            data.name = item.substring(match[0].length);
+        }
+        data.name = data.name.replace(rollRemoveRegex, '').trim()
+    } else {
+        data.name = item
+    }
+    return data
+}
